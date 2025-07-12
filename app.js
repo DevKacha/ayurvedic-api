86441d7ae34173cfe815fc016a798753d4b97895
DELL <your.email@example.com>
2025-07-12 20:25:27 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const Remedy = require('./models/Remedy');
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/ayurvedicdb', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -33,4 +34,4 @@ app.get('/api/remedies', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3001'));
+app.listen(3001, () => console.log('Server running on http://localhost:3001'));
