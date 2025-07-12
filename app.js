fd829ecc1ec7b1edd2f1886beb53975bcce56a4e
DELL <your.email@example.com>
2025-07-12 20:45:32 +0530

    Fix: Added CORS and updated remedy model

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const cors = require('cors');
 
 const Remedy = require('./models/Remedy');
 
-app.use(cors());
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
