265e2bd5330916eed57c91faaecb41993a2ce71e
DELL <your.email@example.com>
2025-07-12 20:43:00 +0530

    Fix: Added CORS and updated remedy model

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
+
 const Remedy = require('./models/Remedy');
 
+app.use(cors());
 const app = express();
 app.use(express.json());
 
