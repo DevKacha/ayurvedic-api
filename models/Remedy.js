28a43198ababd7172bf557df44de6b6390bff8d0
DELL <your.email@example.com>
2025-08-01 00:08:28 +0530

    Updated code with new features

--- models/Remedy.js
+++ models/Remedy.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose');
 
 const remedySchema = new mongoose.Schema({
-    remedy_id: { type: String, required: true, unique: true },
+    remedyId: { type: String, required: true, unique: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
     ingredients: { type: [String], required: true },
-    preparation_steps: { type: String, required: true },
+    preparationSteps: { type: String, required: true },
     usage: { type: String, required: true },
     ailment: { type: String, required: true },
     precautions: { type: String },
     category: { type: String },
     dosage: { type: String },
-    age_group: { type: String },
-    image_url: { type: String },
-    source_reference: { type: String },
-    is_verified: { type: Boolean, default: false },
+    ageGroup: { type: String },
+    imageUrl: { type: String },
+    sourceReference: { type: String },
+    isVerified: { type: Boolean, default: false },
     language: { type: String, default: 'English' },
-    tags: [String]
+    tags: [String],
+    tip: { type: String },
+    type: { type: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Remedy', remedySchema);
